4169471070e705aa4f418d9b48b9297705c6aa6c
Joseph <tan.joseph.gabriel@gmail.com>
2019-12-24 01:29:08 +0800

    Setup Navigation

--- app/src/main/java/com/jgt/pos/ui/productscreen/ProductScreenActivity.java
+++ app/src/main/java/com/jgt/pos/ui/productscreen/ProductScreenActivity.java
@@ -1,10 +1,15 @@
 package com.jgt.pos.ui.productscreen;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.jgt.pos.R;
+import com.jgt.pos.ui.admin.AdminActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class ProductScreenActivity extends AppCompatActivity {
 
@@ -12,5 +17,28 @@ public class ProductScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_screen);
+        Toolbar toolbar = findViewById(R.id.tb_activity_product);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_product, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_product_admin:
+                //TODO: check for password first
+                this.startActivity(new Intent(this, AdminActivity.class));
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
