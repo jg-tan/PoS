8ab8b0aa84cac93b14b2de4ba8d85a850235651e
Joseph <tan.joseph.gabriel@gmail.com>
2019-12-23 23:39:05 +0800

    Setup Adapters

---
+++ app/src/main/java/com/jgt/pos/database/itemdb/ProductItem.java
@@ -0,0 +1,7 @@
+package com.jgt.pos.database.itemdb;
+
+import androidx.room.Entity;
+
+@Entity
+public class ProductItem {
+}
---
+++ app/src/main/java/com/jgt/pos/database/salesdb/SalesItem.java
@@ -0,0 +1,7 @@
+package com.jgt.pos.database.salesdb;
+
+import androidx.room.Entity;
+
+@Entity
+public class SalesItem {
+}
