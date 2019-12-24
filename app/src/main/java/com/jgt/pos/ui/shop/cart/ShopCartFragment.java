2d5a14a10483997bd28b6bbcac4cdf7b5eea384e
Joseph <tan.joseph.gabriel@gmail.com>
2019-12-24 23:50:39 +0800

    Added Cart UI xml
    TODO:
    1. DATABASE methods
    2. Add Item
    3. Edit Item
    4. Item List
    5. Item Grid

--- app/src/main/java/com/jgt/pos/ui/shop/cart/ShopCartFragment.java
+++ app/src/main/java/com/jgt/pos/ui/shop/cart/ShopCartFragment.java
@@ -1,5 +1,6 @@
 package com.jgt.pos.ui.shop.cart;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -7,18 +8,34 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.jgt.pos.R;
+import com.jgt.pos.utils.TestUtils;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ShopCartFragment extends Fragment {
+
+    private RecyclerView rvListView;
+    private LinearLayoutManager layoutManager;
+    private ShopCartListAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Log.d("GAB", "CART");
         View root = inflater.inflate(R.layout.fragment_shop_cart, container, false);
+        Activity activity = getActivity();
+        rvListView = root.findViewById(R.id.shop_cart_fragment_rv_listview);
+        adapter = new ShopCartListAdapter();
+        layoutManager = new LinearLayoutManager(activity);
+        adapter.setCartItems(TestUtils.getTestItemList());
+        rvListView.setHasFixedSize(true);
+        rvListView.setLayoutManager(layoutManager);
+        rvListView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
         return root;
     }
 }
--- app/src/main/java/com/jgt/pos/ui/shop/cart/ShopCartListAdapter.java
+++ app/src/main/java/com/jgt/pos/ui/shop/cart/ShopCartListAdapter.java
@@ -3,13 +3,20 @@ package com.jgt.pos.ui.shop.cart;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.jgt.pos.R;
+import com.jgt.pos.database.cartdb.Cart;
+
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ShopCartListAdapter extends RecyclerView.Adapter<ShopCartListAdapter.ProductCartListItemViewHolder> {
+
+    private List<Cart> cartItems;
 
     @NonNull
     @Override
@@ -22,18 +29,48 @@ public class ShopCartListAdapter extends RecyclerView.Adapter<ShopCartListAdapte
 
     @Override
     public void onBindViewHolder(@NonNull ProductCartListItemViewHolder holder, int position) {
-        //TODO: Think if how to handle CART object
+        holder.setCartItem(cartItems.get(position));
     }
 
     @Override
     public int getItemCount() {
+        if (null != cartItems) {
+            return cartItems.size();
+        }
         return 0;
     }
 
+    public void setCartItems(List<Cart> cartItems) {
+        this.cartItems = cartItems;
+    }
+
+    //TODO: change to live data and add click listeners
     class ProductCartListItemViewHolder extends RecyclerView.ViewHolder {
+        Cart cartItem;
+        TextView tvItemName, tvItemPriceSingle, tvItemPriceTotal, tvItemQty;
+        Button btnAdd, btnRemove, btnDelete;
 
         public ProductCartListItemViewHolder(@NonNull View itemView) {
             super(itemView);
+            this.tvItemName = itemView.findViewById(R.id.cart_tv_item_name);
+            this.tvItemPriceSingle = itemView.findViewById(R.id.cart_tv_item_price_single);
+            this.tvItemPriceTotal = itemView.findViewById(R.id.cart_tv_item_price_total);
+            this.tvItemQty = itemView.findViewById(R.id.cart_tv_item_qty);
+            this.btnAdd = itemView.findViewById(R.id.cart_btn_add);
+            this.btnRemove = itemView.findViewById(R.id.cart_btn_remove);
+            this.btnDelete = itemView.findViewById(R.id.cart_btn_delete);
+        }
+
+        public void setCartItem(Cart cartItem) {
+            this.cartItem = cartItem;
+            String name = cartItem.getName();
+            int priceSingle = cartItem.getPrice();
+            int qty = cartItem.getQuantity();
+            int priceTotal = priceSingle*qty;
+            tvItemName.setText(name);
+            tvItemPriceSingle.setText(Integer.toString(priceSingle));
+            tvItemPriceTotal.setText(Integer.toString(priceTotal));
+            tvItemQty.setText(Integer.toString(qty));
         }
     }
 }
