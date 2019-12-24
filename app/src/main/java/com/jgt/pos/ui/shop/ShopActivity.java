e817603caa3d55df5236c0cce4a8df74476da241
Joseph <tan.joseph.gabriel@gmail.com>
2019-12-24 16:23:10 +0800

    Fixed Navigation for SHOP page Added close for ADMIN page

--- app/src/main/java/com/jgt/pos/ui/shop/ShopActivity.java
+++ app/src/main/java/com/jgt/pos/ui/shop/ShopActivity.java
@@ -7,11 +7,9 @@ import android.view.MenuItem;
 
 import com.jgt.pos.R;
 import com.jgt.pos.ui.admin.AdminActivity;
-import com.jgt.pos.ui.shop.cart.ShopCartFragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
