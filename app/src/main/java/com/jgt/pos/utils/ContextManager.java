8e8f08f7b5aeecc888b9562ce645cf4464946b58
Joseph <tan.joseph.gabriel@gmail.com>
2019-12-25 12:46:26 +0800

    Added Grid Adapter container
    TODO:
    - Database methods
    - Admin Add Item Logic
    - Admin Edit Item UI/Logic
    - Admin List Logic
    - Shop Cart Logic
    - Shop Grid UI/Logic
    BUGS:
    - Adjust views to accommodate target device

--- app/src/main/java/com/jgt/pos/utils/ContextManager.java
+++ app/src/main/java/com/jgt/pos/utils/ContextManager.java
@@ -3,6 +3,7 @@ package com.jgt.pos.utils;
 import android.app.Application;
 
 //TODO: Adjust text size of PRICE if exceeds boundaries
+//TODO: Adjust VIEWS for tablet
 public class ContextManager extends Application {
     private static ContextManager instance;
 
