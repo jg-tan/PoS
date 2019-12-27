4a49107a32902e09e901c4ececa34bea8240f831
Joseph <tan.joseph.gabriel@gmail.com>
2019-12-27 13:59:08 +0800

    Added Grid Fragment UI
    TODO:
    - Database methods
    - Admin Add Item Logic
    - Admin Edit Item UI/Logic
    - Admin List Logic
    - Shop Cart Logic
    - Shop Grid Logic
    - Adjust Dimensions for tablet
    > Add Product
    > Product List

--- app/src/main/java/com/jgt/pos/ui/admin/addproduct/AddProductFragment.java
+++ app/src/main/java/com/jgt/pos/ui/admin/addproduct/AddProductFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.jgt.pos.R;
 
@@ -13,10 +16,30 @@ import androidx.fragment.app.Fragment;
 
 public class AddProductFragment extends Fragment {
 
+    private EditText etName, etPrice;
+    private Button btnAddIcon, btnAddItem;
+    private ImageView ivIcon;
+    private View rootView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.admin_fragment_add_product, container, false);
-        return root;
+        rootView = inflater.inflate(R.layout.admin_fragment_add_product, container, false);
+        this.etName = rootView.findViewById(R.id.admin_add_et_item_name);
+        this.etPrice = rootView.findViewById(R.id.admin_add_et_item_price);
+        this.btnAddIcon = rootView.findViewById(R.id.admin_add_btn_add_icon);
+        this.btnAddItem = rootView.findViewById(R.id.admin_add_btn_add_item);
+        this.ivIcon = rootView.findViewById(R.id.admin_add_iv_item_icon);
+        btnAddIcon.setOnClickListener(this::onAddIconClicked);
+        btnAddItem.setOnClickListener(this::onAddItemClicked);
+        return rootView;
+    }
+
+    public void onAddIconClicked(View view) {
+
+    }
+
+    public void onAddItemClicked(View view) {
+
     }
 }
--- app/src/main/java/com/jgt/pos/ui/shop/grid/ShopGridFragment.java
+++ app/src/main/java/com/jgt/pos/ui/shop/grid/ShopGridFragment.java
@@ -1,5 +1,6 @@
 package com.jgt.pos.ui.shop.grid;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -7,18 +8,41 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.jgt.pos.R;
+import com.jgt.pos.ui.admin.productlist.ProductListAdapter;
+import com.jgt.pos.utils.TestUtils;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ShopGridFragment extends Fragment {
+
+    private RecyclerView rvListView;
+    private GridLayoutManager layoutManager;
+    private ShopGridAdapter adapter;
+    private View rootView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Log.d("GAB", "GRID");
-        View root = inflater.inflate(R.layout.shop_fragment_grid, container, false);
-        return root;
+        rootView = inflater.inflate(R.layout.shop_fragment_grid, container, false);
+        initRecyclerView();
+        return rootView;
+    }
+
+    private void initRecyclerView() {
+        Activity activity = getActivity();
+        rvListView = rootView.findViewById(R.id.shop_grid_fragment_rv_item_list);
+        adapter = new ShopGridAdapter();
+        layoutManager = new GridLayoutManager(activity, 4);
+        adapter.setProductList(TestUtils.getTestProductList());
+        rvListView.setHasFixedSize(true);
+        rvListView.setLayoutManager(layoutManager);
+        rvListView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 }
