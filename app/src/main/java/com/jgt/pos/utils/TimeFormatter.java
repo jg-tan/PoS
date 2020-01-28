89880075bd7917259fb9bed15475114eedc6b2fc
Joseph <tan.joseph.gabriel@gmail.com>
2020-01-29 00:12:55 +0800

    Sales History
    TODO:
    - Export to CSV
    - Edit Fragment
    - Checkout Total price

---
+++ app/src/main/java/com/jgt/pos/utils/TimeFormatter.java
@@ -0,0 +1,19 @@
+package com.jgt.pos.utils;
+
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeFormatter {
+    public static String toDate(long timeStamp) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd yyyy");
+        return dateFormat.format(timeStamp);
+    }
+
+    public static long toTimestamp(String date) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date1 = dateFormat.parse(date);
+        return new Timestamp(date1.getTime()).getTime();
+    }
+}
